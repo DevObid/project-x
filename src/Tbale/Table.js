b9c47f5087a9f8e4bd386eb2fcd57f47df5d96a4
obid <mehdiobidobid@gmail.com>
2024-05-18 07:27:14 -0700

    Version 4

--- src/Tbale/Table.js
+++ src/Tbale/Table.js
@@ -1,65 +1,71 @@
 import React, { useState, useEffect } from 'react';
 
 function Table() {
+    const [hours1, setHours1] = useState(0);
+    const [minutes1, setMinutes1] = useState(0);
 
-    const [state, setState] = useState('');
+    const [hours2, setHours2] = useState(0);
+    const [minutes2, setMinutes2] = useState(0);
 
-    const [StateMinute, SetMinute] = useState('');
+    const [price, setPrice] = useState(0);
+    const [totalAmount, setTotalAmount] = useState(0);
 
-    const [state2, setState2] = useState('');
+    const [BreakHours, setBreakHours] = useState(0);
+    const [Breakminutes, setBreakMinutes] = useState(0);
 
-    const [StateMinute2, SetMinute2] = useState('');
 
-    const [Price, SetPrice] = useState('');
+    const [totalHours, setTotalHours] = useState(0);
+    const [totalMinutes, setTotalMinutes] = useState(0);
 
-    const [TotaleAmount, SetTotaleAmount] = useState('');
+    function calculateTotal() {
+        let totalHours = +hours1 + +hours2 - BreakHours;
+        let totalMinutes = +minutes1 + +minutes2 - Breakminutes;
 
-    function SetTotale() {
-        let Totale = +state + +StateMinute / 10 + +state2 + +StateMinute2 / 10
-        SetTotaleAmount(Price * Totale)
+        if (totalMinutes >= 60) {
+            totalHours += Math.floor(totalMinutes / 60);
+            totalMinutes = totalMinutes % 60;
+        }
+
+        setTotalHours(totalHours);
+        setTotalMinutes(totalMinutes);
+        setTotalAmount(price * (totalHours + totalMinutes / 60));
     }
 
     useEffect(() => {
-        SetTotale()
+        calculateTotal();
 
-    }, [state, StateMinute, state2, StateMinute2, Price])
+    }, [hours1, minutes1, hours2, minutes2, price, BreakHours, Breakminutes]);
 
-    const handleChangeHour1 = (event) => {
-        setState(event.target.value);
-
+    const handleChangeHours1 = (event) => {
+        setHours1(event.target.value);
     };
 
-
-    const handleChangeMunit1 = (event2) => {
-        let munit = event2.target.value;
-
-
-        SetMinute(munit);
+    const handleChangeMinutes1 = (event) => {
+        setMinutes1(event.target.value);
     };
 
-    const handleChangeHour2 = (event3) => {
-        let munit = event3.target.value;
+    const handleChangeHours2 = (event) => {
+        setHours2(event.target.value);
+    };
 
-        setState2(munit);
-    }
-
-    const handleChangeMunit2 = (event3) => {
-
-        let munit = event3.target.value;
-
-
-        SetMinute2(munit);
-
-
-
-    }
-
+    const handleChangeMinutes2 = (event) => {
+        setMinutes2(event.target.value);
+    };
 
     const handleChangePrice = (event) => {
+        setPrice(event.target.value);
+    };
 
-        SetPrice(event.target.value)
 
-    }
+    const handleChangeHoursBreak = (event) => {
+        setBreakHours(event.target.value);
+    };
+
+
+    const handleChangeMinutesBreak = (event) => {
+        setBreakMinutes(event.target.value);
+    };
+
 
 
 
@@ -72,27 +78,13 @@ function Table() {
                 <table className="h-[100%] w-[100%] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th className="px-6 py-3 flex item-center justify-center">
-                                Days
-                            </th>
-                            <th className="px-6 py-3">
-                                AM
-                            </th>
-                            <th className="px-6 py-3">
-                                PM
-                            </th>
-                            <th className="px-6 py-3">
-                                BREAK
-                            </th>
-                            <th className="px-6 py-3">
-                                PRICE OF HOURS
-                            </th>
-                            <th className="px-6 py-3">
-                                TOTAL HOURS
-                            </th>
-                            <th className="px-6 py-3">
-                                TOTAL AMOUNT
-                            </th>
+                            <th className="px-6 py-3 flex item-center justify-center">Days</th>
+                            <th className="px-6 py-3">AM</th>
+                            <th className="px-6 py-3">PM</th>
+                            <th className="px-6 py-3">BREAK</th>
+                            <th className="px-6 py-3">PRICE OF HOURS</th>
+                            <th className="px-6 py-3">TOTAL HOURS</th>
+                            <th className="px-6 py-3">TOTAL AMOUNT</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -102,32 +94,27 @@ function Table() {
                             </th>
                             <td className="px-6 py-4">
                                 <div className="h-[100%] w-[100%] flex justify-center items-center">
-                                    <input onChange={handleChangeHour1} className="w-8" placeholder="00" />
-                                    <input onChange={handleChangeMunit1} className="w-8" placeholder="00" />
-
+                                    <input onChange={handleChangeHours1} className="w-8" placeholder="00" />
+                                    <input onChange={handleChangeMinutes1} min={0} type='number' max={59} className="w-8" placeholder="00" />
                                 </div>
                             </td>
                             <td className="px-6 py-4">
-                                <input className="w-8" onChange={handleChangeHour2} placeholder="00" />
-                                <input className="w-8" onChange={handleChangeMunit2} placeholder="00" />
-
+                                <input className="w-8" onChange={handleChangeHours2} placeholder="00" />
+                                <input className="w-8" onChange={handleChangeMinutes2} min={0} type='number' max={59} placeholder="00" />
                             </td>
                             <td className="px-6 py-4">
-                                <input className="w-8" placeholder="00" />
-                                <input className="w-8" placeholder="00" />
-
+                                <input className="w-8" onChange={handleChangeHoursBreak} placeholder="00" />
+                                <input className="w-8" onChange={handleChangeMinutesBreak} min={0} type='number' max={59} placeholder="00" />
                             </td>
                             <td className="px-6 py-4">
-                                <input className='w-8' onChange={handleChangePrice} placeholder=' 00$' />
+                                <input className="w-8" onChange={handleChangePrice} placeholder="00$" />
                             </td>
                             <td className="px-6 py-4">
-                                {+state + +StateMinute / 10 + +state2 + +StateMinute2 / 10}
-
+                                HOURS {totalHours} AND {totalMinutes} MINUTES
                             </td>
-                            <td>
-                                {TotaleAmount}
+                            <td className="px-6 py-4">
+                                {totalAmount}
                             </td>
-
                         </tr>
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -136,17 +123,14 @@ function Table() {
                             <td className="px-6 py-4">
                                 <input className="w-8" placeholder="00" />
                                 <input className="w-8" placeholder="00" />
-
                             </td>
                             <td className="px-6 py-4">
                                 <input className="w-8" placeholder="00" />
                                 <input className="w-8" placeholder="00" />
-
                             </td>
                             <td className="px-6 py-4">
                                 <input className="w-8" placeholder="00" />
                                 <input className="w-8" placeholder="00" />
-
                             </td>
                             <td className="px-6 py-4">
                                 price
@@ -154,7 +138,7 @@ function Table() {
                             <td className="px-6 py-4">
                                 hour
                             </td>
-                            <td>
+                            <td className="px-6 py-4">
                                 jjj
                             </td>
                         </tr>
@@ -165,17 +149,14 @@ function Table() {
                             <td className="px-6 py-4">
                                 <input className="w-8" placeholder="00" />
                                 <input className="w-8" placeholder="00" />
-
                             </td>
                             <td className="px-6 py-4">
                                 <input className="w-8" placeholder="00" />
                                 <input className="w-8" placeholder="00" />
-
                             </td>
                             <td className="px-6 py-4">
                                 <input className="w-8" placeholder="00" />
                                 <input className="w-8" placeholder="00" />
-
                             </td>
                             <td className="px-6 py-4">
                                 prie
@@ -183,14 +164,10 @@ function Table() {
                             <td className="px-6 py-4">
                                 hour
                             </td>
-                            <td>
+                            <td className="px-6 py-4">
                                 jjj
                             </td>
-
-
                         </tr>
-
-
                     </tbody>
                 </table>
             </div>
